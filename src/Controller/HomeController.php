dd8b4693757c740cbef3bacebeb5c7dae700a42f
Tchondo Tchosso <edisontchondo@gmail.com>
2021-06-07 16:06:12 +0200

    creation de la card avec bootstrap

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Articles;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,18 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
+
+    //je recupere tout les articles dans ma base de donné
     public function index(): Response
     {
-        return $this->render("./home/index.html.twig");
+        $repo = $this->getDoctrine()->getRepository(Articles::class);
+        $articles = $repo->findAll();
+        return $this->render("./home/index.html.twig", [
+            'articles' => $articles,
+        ]);
     }
+    /* public function index(): Response
+    {
+        return $this->render("./home/index.html.twig");
+    }*/
 }
